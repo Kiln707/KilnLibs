4964e5173a4e844ccc5e75a75e7ec449415ce3b0
kiln707 <steven.guy.swanson@gmail.com>
2017-12-15 10:26:10 -0800

    Added Base Networking
    Created base networking utilizing SerializationTag.
    TODO:
    Add network handling for server/client
    Add Network Server

---
+++ kilnlibs/network.py
@@ -0,0 +1,46 @@
+from .serializationTag import SerializationTag
+import sys, struct, select
+
+def sendNetworkData(connection, tag):
+    if isinstance(tag, Tag):
+        connection.sendall(struct.pack('>i', len(data))+SerializationTag.encodePickle(tag))
+    else:
+        raise TypeError('Value tag, needs to be of Tag object.')
+
+def receiveNetworkData(connection):
+    #data length is packed into 4 bytes
+    total_len=0;total_data=bytearray();size=sys.maxsize
+    sock_data=bytearray();recv_size=8192
+    while total_len<size:
+        sock_data=connection.recv(recv_size)
+        if not sock_data:
+            return None
+        if not total_data:
+            if len(sock_data)>4:
+                size=struct.unpack('>i', sock_data[:4])[0]
+                for b in sock_data[4:]:
+                    total_data.append(b)
+            elif len(sock_data) == 4:
+                size=struct.unpack('>i', sock_data[:4])[0]
+        else:
+            total_data.append(sock_data)
+        total_len=len(total_data)
+    return SerializationTag.decodePickle(bytes(total_data))
+
+def initializeNetworkServer(address, port, connections=5):
+    print("Creating Network server listening on "+address+":"port)
+    if address and port:
+        networkSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            networkSocket.bind( address, port )
+            networkSocket.listen(connections)
+            print("Network server creation complete.")
+            return networkSocket
+        except socket.error as msg:
+            print("Failed to bind to"+address+":"+port,"Error Code:", str(msg[0]), 'Message:',msg[1])
+    else:
+        print('Failed to initialize network server. Bad address, port')
+    return None
+
+def initializeNetworkConnection(address, port):
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect( (address, port) )
